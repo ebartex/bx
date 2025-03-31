dbf79f26ae8e5f349a4f2cf958be7d7fbdd6f4ce
ebartex <biuro.bartex@gmail.com>
2025-03-31 19:31:22 +0200

    product view

--- src/components/layout/navbar/MenuMobile.tsx
+++ src/components/layout/navbar/MenuMobile.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { Menu } from "lucide-react";
-import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet,  SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 
