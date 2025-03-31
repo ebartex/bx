71764daa6cc82eb7bf70986c39091c9494f62873
ebartex <biuro.bartex@gmail.com>
2025-03-31 19:29:56 +0200

    product view

--- src/components/layout/navbar/MenuMobile.tsx
+++ src/components/layout/navbar/MenuMobile.tsx
@@ -1,54 +1,87 @@
 "use client";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 import { Menu } from "lucide-react";
-
 import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 
 export default function MenuMobile() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false); // Stan do kontrolowania otwarcia menu
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [categories, setCategories] = useState<string[]>([]); // State to store categories
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen); // Zmienia stan menu
+    setIsMenuOpen(!isMenuOpen); // Toggle menu state
   };
+
+  useEffect(() => {
+    // Fetch the categories from the API
+    fetch("https://www.bapi2.ebartex.pl/xt/index?Xt-super=2200&Xt-root=2200")
+      .then(response => response.json())
+      .then(data => {
+        // Assuming data is an array of categories
+        setCategories(data); // Set categories state
+      })
+      .catch(error => {
+        console.error("Error fetching categories:", error);
+      });
+  }, []);
 
   return (
     <>
-      {/* Ikona Menu */}
- 
-
-      {/* Sheet komponent - menu wysuwane z lewej strony */}
       <Sheet>
-      <SheetTrigger asChild>
-      <Menu
-        className="sm:hidden"
-        onClick={toggleMenu} // Kliknięcie powoduje otwarcie/zamknięcie menu
-      />
-      
-      </SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Edit profile</SheetTitle>
-          <SheetDescription>
-            Make changes to your profile here. Click save when you
-          </SheetDescription>
-        </SheetHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
+        <SheetTrigger asChild>
+          <Menu
+            className="sm:hidden"
+            onClick={toggleMenu}
+          />
+        </SheetTrigger>
 
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
+        <SheetContent className="p-0">
+          <SheetHeader className="p-0">
+            <SheetTitle className="pt-4 pl-2">Kategorie</SheetTitle>
+  
+          </SheetHeader>
 
+          <div className="p-0 overflow-auto ">
+            {/* Display categories */}
+            <div>
+              {categories.map((category, index) => (
+                <div key={index} className="">
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1" className="border-b border-slate-200">
+            <AccordionTrigger className="pl-4 pr-4 ">{category.kod}</AccordionTrigger>
+            <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+            Yes. It adheres to the WAI-ARIA design pattern.<br/>
+          
+        </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-        <SheetFooter>
-          <SheetClose asChild>
-            <Button type="submit">Save changes</Button>
-          </SheetClose>
-        </SheetFooter>
-      </SheetContent>
-    </Sheet>
+
+   
+        </SheetContent>
+      </Sheet>
     </>
   );
 }
