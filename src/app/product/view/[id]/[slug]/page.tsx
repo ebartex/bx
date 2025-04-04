87c9b01b2c5179e8b85f0e202ea1963259e540be
ebartex <biuro.bartex@gmail.com>
2025-04-04 22:28:26 +0200

    command searchddd

--- src/app/product/view/[id]/[slug]/page.tsx
+++ src/app/product/view/[id]/[slug]/page.tsx
@@ -1,8 +1,13 @@
 import ProductPage from "./Productpage";
-
+interface PageProps {
+    params: {
+      id: string;
+      slug: string;
+    };
+  }
 
 // (1) Dynamic metadata
-export async function generateMetadata({ params }: { params: { id: string } }) {
+export async function generateMetadata({ params }: PageProps) {
   const res = await fetch(`https://www.bapi2.ebartex.pl/tw/index?tw-id=${params.id}`);
   const data = await res.json();
 
