eb3b4feffad667b5000241622f9a98f55e8de7f5
ebartex <biuro.bartex@gmail.com>
2025-04-05 11:19:13 +0200

    product view

--- src/app/szukaj/SearchResults.tsx
+++ src/app/szukaj/SearchResults.tsx
@@ -97,12 +97,12 @@ export default function SearchResults() {
                 </div>
                 <h2 className="text-sm text-zinc-800 font-normal mb-2">{product.nazwa}</h2>
                 
-                {/* Wstawiamy cenę po lewej stronie, upewniając się, że cena jest liczbą */}
-                {cena > 0 && (
+                {/* Wstawiamy cenę po lewej stronie */}
+                {cena > 0 ? (
                   <div className="text-sm text-zinc-700 mb-2">
                     Cena: <span className="font-bold">{cena} PLN</span>
                   </div>
-                )}
+                ) : null}
 
                 {/* Ikona + napis w lewym dolnym rogu */}
                 <div className="absolute bottom-0 left-0 p-2 flex items-center">
