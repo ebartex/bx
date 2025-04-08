4261ba013e1d11a64ff57364e94e0d94aa630240
ebartex <biuro.bartex@gmail.com>
2025-04-08 12:16:51 +0200

    product view

--- src/app/categories/view/[id]/[slug]/layout.tsx
+++ src/app/categories/view/[id]/[slug]/layout.tsx
@@ -2,19 +2,51 @@
 
 'use client';
 
-import { ReactNode } from 'react';
-
+import { ReactNode, useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
 
 import MenuDesktop from '@/components/layout/sidebar/MenuDesktop';
 
 interface LayoutProps {
   children: ReactNode; // Zawartość strony (komponenty, tekst itp.)
 }
-
+interface Category {
+  kod: string;
+}
 export default function CategoryLayout({ children }: LayoutProps) {
+  const { id } = useParams();
+  const [categoryName, setCategoryName] = useState<string | null>(null);
+  // Funkcja metadata do dynamicznego ustawiania tytułu
+  useEffect(() => {
+    if (id) {
+      fetch(`https://www.bapi2.ebartex.pl/xt/index?xt-id=${id}`)
+        .then((res) => res.json())
+        .then((data: Category[]) => {
+          if (Array.isArray(data) && data.length > 0) {
+            setCategoryName(data[0].kod);
+          }
+        })
+        .catch((err) => {
+          console.error("Błąd pobierania kategorii:", err);
+        });
+    }
+  }, [id]);
 
 
-  return (
+
+  const metadata = categoryName
+    ? { title: `${categoryName} - Moja Strona` }
+    : { title: "Ładowanie..." };
+
+
+        return (
+           <>
+          {/* Dodajemy meta dane do head */}
+          <head>
+        <meta name="description" content={categoryName ? `${categoryName} - Kategoria produktów` : "Ładowanie kategorii"} />
+        <title>{metadata.title}</title>
+      </head>
+ 
     <div className="flex">
       {/* Menu boczne (widoczne tylko na większych ekranach) */}
       <div className="hidden lg:block">
@@ -29,5 +61,6 @@ export default function CategoryLayout({ children }: LayoutProps) {
         </div>
       </div>
     </div>
+    </>
   );
 }
