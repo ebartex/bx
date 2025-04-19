2183f1378b3e4847bc91e1c2d346dfc01815a99d
ebartex <biuro.bartex@gmail.com>
2025-04-19 10:45:48 +0200

    product view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "@/styles/globals.css";
 import Navbar from "@/components/layout/navbar/page";
 import {Roboto} from 'next/font/google';
 import Topbar from "@/components/layout/topbar/page";
-
+import Footer from "@/components/layout/footer/page";
 
 const roboto = Roboto({
   weight: ["300", "400", "500", "700", "900"],
@@ -24,15 +24,20 @@ export default function RootLayout({
       <head>
         <title>Bartex Gorzkowice telefon 44 6818 043 Piotr Bartnik Materiały Budowlane</title>
       </head>
-      <body className="flex flex-col min-h-screen">
-        <Topbar />
-        <div className="xl:w-[1200px] mx-auto flex-grow">
+      <body >
+      <Topbar />
+        <div className="xl:w-[1200px] mx-auto">
           <Navbar />
-          {children}
-        
+        {children}
         </div>
-        
+
+        <div className="xl:w-[1200px] mx-auto">
+          <Footer />
+        </div>
+
       </body>
     </html>
   );
 }
+
+
