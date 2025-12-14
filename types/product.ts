3480fc6b26c6fbda7de19c0164feff317fdbc474
ebartex <biuro.bartex@gmail.com>
2025-12-14 08:56:51 +0100

    nowa odslona strony

--- types/product.ts
+++ types/product.ts
@@ -3,9 +3,7 @@
 // Główny produkt – DOPASUJ POLA do odpowiedzi z cakeapi/tw/index
 export type Product = {
   jm1: string;
-  s_t_elements: any;
   jmdod1: string;
-  cn: any;
   sm: any;
   jm: string;
   kodpaskowy?: string;
@@ -15,16 +13,33 @@ export type Product = {
   kod: string;
   katalog: string;
   productphoto: { id: number; tw_id: number; photo_512: string; photo_256: string; photo_128: string; main_photo: number }[];
-  // tu dodaj resztę pól, które zwraca twój backend
-  // [key: string]: any; // możesz dodać fallback, jeśli chcesz
+  zp: {
+    data: string;
+    id?: string;
+  }[];
+  cn?: { cena: string, cena1?: string, cena2?: string }[];  // Cena produktu
+  s_t_elements?: STElement[];   
 }
-
+export type ProductClassification {
+  ElementId: number;
+  CDim_jm_Val: string;
+  CDim_jm_shop: string;
+  CDim_przeljmdod3: string;
+}
+export type STElement = {
+  ElementId: string;
+  Shortcut: string;
+  product_classification: ProductClassification[]
+}
 export type ProductCatalog = {
   tw: never[];
   kod: string;
 }
 
-
+export type ProductPhoto = {
+  main_photo: number;
+  photo_512: string;
+}
 export type OrderHistory = {
   ilosc: number;
 }
