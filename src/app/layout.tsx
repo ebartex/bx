ff13d667aaca1bd8777d173575c2d0c5c8f2ad00
ebartex <biuro.bartex@gmail.com>
2025-12-21 20:51:49 +0100

    nowa odslona strony

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { Poppins } from "next/font/google";
 import Topbar from "@/components/layout/topbar/page";
 import Footer from "@/components/layout/footer/page";
 import ChristmasTopbar from "@/components/layout/topbar/ChristmasTopbar";
+import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const poppins = Poppins({
   weight: ["300", "400", "500", "700", "900"],
@@ -14,54 +16,42 @@ const poppins = Poppins({
 
 export const metadata = {
   title: "Bartex Gorzkowice telefon 44 6818 043 Piotr Bartnik Materiały Budowlane",
-  description: "Bartex Gorzkowice materiały budowlane, chemia budowlana, narzędzia, farby, lakiery, kleje, izolacje, systemy dociepleń, artykuły hydrauliczne i elektryczne.",
+  description:
+    "Bartex Gorzkowice materiały budowlane, chemia budowlana, narzędzia, farby, lakiery, kleje, izolacje, systemy dociepleń, artykuły hydrauliczne i elektryczne.",
 };
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pl" className={poppins.className}>
-      <head>
-                <script
+      <body className="bg-stone-100">
+        {/* Google Tag Manager - bez inline script w <head> */}
+        <GoogleTagManager gtmId="GTM-53LWG4GN" />
+
+        {/* Adsense - jako next/script, też bez wpychania ręcznie do <head> */}
+        <Script
           async
+          strategy="afterInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6585756983159019"
           crossOrigin="anonymous"
         />
 
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-53LWG4GN');
-            `,
-          }}
-        />
-      </head>
-      <body className="bg-stone-100">
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-53LWG4GN"
             height="0"
             width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
+            style={{ display: "none", visibility: "hidden" }}
+          />
         </noscript>
-        
+
         <ChristmasTopbar />
         <Topbar />
+
         <div className="bg-white">
           <Navbar />
         </div>
-        <div className="xl:w-[1200px] mx-auto">
-    
-          {children}
-        </div>
+
+        <div className="xl:w-[1200px] mx-auto">{children}</div>
 
         <div className="xl:w-[1200px] mx-auto">
           <Footer />
