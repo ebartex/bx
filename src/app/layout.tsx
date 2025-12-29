e3feff45268a21a1ad015a6e8af77d7c7689bf18
ebartex <biuro.bartex@gmail.com>
2025-12-29 23:23:17 +0100

    remove bearer 11

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/layout/navbar/page";
 import { Poppins } from "next/font/google";
 import Topbar from "@/components/layout/topbar/page";
 import Footer from "@/components/layout/footer/page";
-
+import RatingBar from "@/components/rating/RatingBar";
 import NewYearTopbar from "@/components/layout/topbar/NewYearTopbar";
 
 
@@ -29,29 +29,6 @@ export default function RootLayout({
   
   return (
     <html lang="pl" className={poppins.className}>
-      <head>
-                <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6585756983159019"
-          crossOrigin="anonymous"
-        />
-
-        {/* Google Tag Manager */}
-        
-    
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-53LWG4GN');
-            `,
-          }}
-        />
-      </head>
       <body className="bg-stone-100">
         <noscript>
           <iframe
@@ -71,7 +48,7 @@ export default function RootLayout({
         <div className="xl:w-[1200px] mx-auto">
     
           {children}
-   
+          <RatingBar />
         </div>
 
         <div className="xl:w-[1200px] mx-auto">
