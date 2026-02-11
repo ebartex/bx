8834b32d2e7bc6836b442fe8269a0225f58b900d
ebartex <biuro.bartex@gmail.com>
2026-02-11 09:39:48 +0100

    merge deploy/vercel into main

--- src/components/layout/category/CategoryHeader.tsx
+++ src/components/layout/category/CategoryHeader.tsx
@@ -10,7 +10,7 @@ export default function CategoryHeader() {
   return (
     <div className="px-4 pt-2 pb-4">
       <h1 className="text-xl font-normal">
-        <span className="font-semibold">{name}</span>
+        <span className="dark:font-semibold font-medium">{name}</span>
       </h1>
     </div>
   );
